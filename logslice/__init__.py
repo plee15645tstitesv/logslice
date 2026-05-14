369c706e57d6ad044251f0ed0a468c6b00b84115
Pete Stites <plee15645tstitesv@users.noreply.github.com>
2026-05-15 02:42:43 +0800

    Add __version__ attribute to package namespace

--- logslice/__init__.py
+++ logslice/__init__.py
@@ -3,8 +3,11 @@
 from logslice.log_archive import LogArchive
 from logslice.slicer import slice_archive, slice_path
 
+__version__ = "0.1.0"
+
 __all__ = [
     "LogArchive",
     "slice_archive",
     "slice_path",
+    "__version__",
 ]
